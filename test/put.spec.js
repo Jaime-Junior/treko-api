a25b9e959ae3c73a29af1f07f922d9b3111a0d0a
Jaime-Junior <58405708+Jaime-Junior@users.noreply.github.com>
2020-06-24 18:07:18 -0300

    Update put.spec.js

--- test/put.spec.js
+++ test/put.spec.js
@@ -1,57 +1,52 @@
-import chai from 'chai';
-import chaiHttp from 'chai-http';
-import tasksModel from '../models/task'
+import chai from "chai";
+import chaiHttp from "chai-http";
+import tasksModel from "../models/task"
+import { Context } from "mocha";
 
 chai.use(chaiHttp);
 
-const app = require('../app');
+const app = require("../app");
 const request = chai.request.agent(app);
 const expect = chai.expect;
 
-describe('put', () => {
-
-    context('quando eu altero uma tarefa', () => {
+describe("put", () => {
+    context("quando eu altero uma tarefa", () => {
 
         let task = {
-            _id: require('mongoose').Types.ObjectId(),
-            title: 'Comprar Fandagos',
-            owner: 'eu@papito.io',
+            _id: require("mongoose").Types.ObjectId(), //cria a massa de testes com um ID pré-definido
+            title: "Comprar fandangos",
+            owner: "eu@papito.io",
             done: false
+
         }
 
         before((done) => {
-            tasksModel.insertMany([task], (error, docs) => {
-                expect(error).to.be.null
-            });
+            tasksModel.insertMany([task]) //insere a tarefa (chamada task) no banco de dados
             done()
         })
+        //put localhost:3000/task/id
 
-        it('entao deve retornar 200', (done) => {
-            task.title = 'Comprar Baconzitos',
-            task.done = true
+        it("então deve retornar 200", (done) => {
+            task.title = "comprar baconzitos", //atualizando dado
+                task.done = true //atualizando dado
             request
-                .put('/task/' + task._id)
-                .send(task)
+                .put("/task/" + task._id) //endereço onde o PUT será feito
+                .send(task) //envia a tarefa
                 .end((err, res) => {
                     expect(res).to.have.status(200)
-                    expect(res.body).to.eql({})
+                    expect(res.body).to.eql({}) //a API está programada para retornar um objeto vazio, portanto este teste valida isso
                     done()
                 })
         })
-
-        it('e deve retornar os dados atualizados', (done) => {
+        it("e deve retornar os dados atualizados", (done) => {
             request
-                .get('/task/' + task._id)
+                .get("/task/" + task._id) //endereço onde o GET será feito
                 .end((err, res) => {
                     expect(res).to.have.status(200)
-                    expect(res.body.data.title).to.eql(task.title)
+                    expect(res.body.data.title).to.eql(task.title) //a API está programada para retornar um objeto vazio, portanto este teste valida isso
                     expect(res.body.data.done).to.be.true
                     done()
                 })
         })
-
-
     })
-
-
 })
